6f322f9a616dfbc35f71b0a55b9455feb72df361
tatetate55 <ruteinyoyokk@gmail.com>
2025-04-29 09:19:19 +0900

    Initial Commit

---
+++ TriviaGuideMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaGuideMap
+//
+//  Created by KAMAKURAKAZUHIRO on 2025/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriviaGuideMap/TriviaGuideMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGuideMapApp.swift
+//  TriviaGuideMap
+//
+//  Created by KAMAKURAKAZUHIRO on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGuideMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
